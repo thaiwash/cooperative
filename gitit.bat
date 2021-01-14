2c5bc558cd6a29e0dc6bf99865dd6c9f59fd445f
Taivas <taivas.gogoljuk@gmail.com>
2021-01-14 17:28:32 +0200

    autogit

--- gitit.bat
+++ gitit.bat
@@ -1,5 +1,5 @@
-git onfig --global user.email taivas.gogoljuk@gmail.com
-git onfig --global user.name Taivas Gogoljuk
+git config --global user.email taivas.gogoljuk@gmail.com
+git config --global user.name Taivas Gogoljuk
 git add *
 git commit -m "autogit"
 git push
